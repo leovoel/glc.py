0dc664ee1ae06ea2270f6d9010f2b83e37791a9e
leovoel <knarkleo@live.com>
2016-03-31 19:42:46 -0300

    add get_extents for Text shapes
    this simply calls cairo_context.font_extents() and cairo_context.text_extents(...).
    it returns the results inside a dict. the keys that correspond to the values returned
    by .font_extents are:
    
        ascent
        descent
        font_height
        max_x_advance
        max_y_advance
    
    and the ones returned by .text_extents are:
    
        x_bearing
        y_bearing
        text_width
        text_height
        x_advance
        y_advance
    
    see the pycairo documentation for more details on what these values stand for.
    
    http://cairographics.org/documentation/pycairo/3/reference/context.html#cairo.Context.font_extents
    http://cairographics.org/documentation/pycairo/3/reference/context.html#cairo.Context.text_extents

--- glc/shapes/text.py
+++ glc/shapes/text.py
@@ -16,6 +16,43 @@ import cairo
 
 class Text(Shape):
 
+    def get_extents(self, context):
+        text = self.get_string("text", 0, "Hello world")
+        size = self.get_number("size", 0, 20)
+        family = self.get_string("family", 0, "sans-serif")
+        weight = self.get_string("weight", 0, "normal")
+
+        if weight == "bold":
+            _weight = cairo.FONT_WEIGHT_BOLD
+        else:
+            _weight = cairo.FONT_WEIGHT_NORMAL
+
+        context.save()
+
+        context.select_font_face(family, cairo.FONT_SLANT_NORMAL, _weight)
+        context.set_font_size(size)
+
+        ascent, descent, font_height, max_x_advance, max_y_advance = context.font_extents()
+        x_bearing, y_bearing, width, height, x_advance, y_advance = context.text_extents(text)
+
+        context.restore()
+
+        extents = {
+            "ascent": ascent,
+            "descent": descent,
+            "font_height": font_height,
+            "max_x_advance": max_x_advance,
+            "max_y_advance": max_y_advance,
+            "x_bearing": x_bearing,
+            "y_bearing": y_bearing,
+            "text_width": width,
+            "text_height": height,
+            "x_advance": x_advance,
+            "y_advance": y_advance,
+        }
+
+        return extents
+
     def draw(self, context, t):
         x = self.get_number("x", t, 100)
         y = self.get_number("y", t, 100)
