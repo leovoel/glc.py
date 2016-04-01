04ca7d6d6b5f092d39020a30ce5e0892b86f2ee7
leovoel <knarkleo@live.com>
2016-03-31 22:14:10 -0300

    update marquee.py

--- examples/marquee.py
+++ examples/marquee.py
@@ -3,15 +3,24 @@ from glc import Gif
 
 
 with Gif(get_filename(__file__)) as a:
-    a.set_ease("linear").set_loop(False).set_duration(8).set_size(400, 100)
+    a.set_ease("linear").set_loop(False).set_duration(4).set_size(400, 100)
     l = a.render_list
     t = l.text(
         x=a.w * 0.5, y=a.h * 0.5,
         family="Comic Sans MS",
         size=30,
-        text="this is text",
+        text="text test text test",
         translation_y=0,
     )
 
     ex = t.get_extents(a.context)
-    t.set_prop(translation_x=[ex['text_width'] * 2, ex['text_width'] * -2])
+    tw = ex["text_width"]
+
+    # the proper way to put the text at the edge of the canvas is:
+    # animation width / 2 + text width / 2
+    # for the other way around, invert the signs, so
+    # -animation width / 2 - text width / 2
+
+    t.set_prop(
+        translation_x=[a.w * 0.5 + tw * 0.5, -a.w * 0.5 - tw * 0.5]
+    )
